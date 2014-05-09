08550dc6634d9d36508b4b9edb7e1d9d4f26812c
YihuiFu <hqufyh@163.com>
2014-05-09 13:56:24 +0800

    。

--- 推荐系统/RecommendedHelper/RecommendedHelper.ReadDatasetToSQL/main.cs
+++ 推荐系统/RecommendedHelper/RecommendedHelper.ReadDatasetToSQL/main.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Data;
 using System.Data.Sql;
 using System.Data.SqlClient;
@@ -17,36 +18,54 @@ namespace RecommendedHelper.ReadDatasetToSQL
         const int itemTotal = 3952; //物品总数
         const int recommendNum = 10;// 推荐数量
 
+        double recall = 0.0;    //召回率
+        double precision = 0.0;   //准确率
+
         int[,] TrainSet = new int[itemTotal, userTotal];  //训练集
         int[,] TestSet = new int[itemTotal, userTotal]; //测试集
 
-        double [,] SimilarityMatrix=new double[itemTotal,itemTotal];  //相似性矩阵， 存储物品的相似度
+        double[,] SimilarityMatrix = new double[itemTotal, itemTotal];  //相似性矩阵， 存储物品的相似度
         double[,] UserInterestMatrix = new double[userTotal, itemTotal];  // 用户兴趣物品矩阵
 
         int[,] RecommendSet = new int[userTotal, recommendNum];// 存储用户推荐结果
+        struct simi
+        {
+            double value;  //相似值
+            int num;   // 相似物品号
+        };
+
+        simi [,]OrderedSimilarityMatrix=new simi[itemTotal,itemTotal];  //排序后的相似性矩阵
 
         /// <summary>
-        /// 将数据集分成训练集 和 测试集
+        /// 将数据集分成训练集 和 测试集  ----------------Success
         /// </summary>
         /// <param name="i"></param>
-        /// <param name="j"></param>
-        public void SplitDataSet(int i,int j)
+        /// <param name="j"></param> 
+        public void SplitDataSet(int i, int j)
         {
+            Array.Clear(TestSet, 0, 3952 * 6040);
+            Array.Clear(TrainSet, 0, 3952 * 6040);
+            Thread.Sleep(1000);
             string sqlStr = "select * from Ratings";
-
             var dataReader = SqlSeverProvider.ExecuteReader(sqlStr);
-            var userID= dataReader.GetInt32(0);
-            var itemID = dataReader.GetInt32(1);
-            var rate = dataReader.GetInt32(2);
-
-            Random rand = new Random(DateTime.Now.Millisecond);
-            if (rand.Next()%(i-1)==j)
+            while (dataReader.Read())
             {
-                TestSet[itemID-1,userID]=1;
-            }
-            else
-            {
-                TrainSet[itemID - 1, userID - 1] = 1;
+                var userID = dataReader.GetInt32(0);
+                var itemID = dataReader.GetInt32(1);
+                var rate = dataReader.GetInt32(2);
+                Random rand = new Random(DateTime.Now.Millisecond);
+                try
+                {
+                    if (rand.Next() % (i - 1) == j)
+                    {
+                        TestSet[itemID - 1, userID] = 1;
+                    }
+                    else
+                    {
+                        TrainSet[itemID - 1, userID - 1] = 1;
+                    }
+                }
+                catch { }
             }
         }
 
@@ -55,7 +74,7 @@ namespace RecommendedHelper.ReadDatasetToSQL
         /// </summary>
         /// <param name="itemA"></param>
         /// <param name="itemB"></param>
-        public double CalculateSimilarityForA_B(int itemA,int itemB)
+        public double CalculateSimilarityForA_B(int itemA, int itemB)
         {
             int commonCount = 0; //物品A 和物品 B都被用户评论的个数
             int ACount = 0;
@@ -63,22 +82,22 @@ namespace RecommendedHelper.ReadDatasetToSQL
             double similarity = 0.0;
             for (int i = 0; i < userTotal; i++)
             {
-                if (TrainSet[itemA,i]>0 && TrainSet[itemB,i]>0)
+                if (TrainSet[itemA, i] > 0 && TrainSet[itemB, i] > 0)
                 {
                     commonCount++;
                 }
-                if (TrainSet[itemA,i]>0)
+                if (TrainSet[itemA, i] > 0)
                 {
                     ACount++;
                 }
-                if (TrainSet[itemB,i]>0)
+                if (TrainSet[itemB, i] > 0)
                 {
                     BCount++;
                 }
             }
 
-            double temp = Math.Sqrt(ACount*BCount);
-            if (temp==0)
+            double temp = Math.Sqrt(ACount * BCount);
+            if (temp == 0)
             {
                 return 0.0;
             }
@@ -90,7 +109,7 @@ namespace RecommendedHelper.ReadDatasetToSQL
         }
 
         /// <summary>
-        /// 计算所有物品之间的相似性，得到相似性矩阵
+        /// 计算所有物品之间的相似性，得到相似性矩阵 --------------Success
         /// </summary>
         public void GetSimilarityMatrix()
         {
@@ -98,9 +117,22 @@ namespace RecommendedHelper.ReadDatasetToSQL
             {
                 for (int j = 0; j < itemTotal; j++)
                 {
-                    SimilarityMatrix[i, j] = CalculateSimilarityForA_B(i,j);
+                    SimilarityMatrix[i, j] = CalculateSimilarityForA_B(i, j);
                 }
             }
+
+            // 输出相似性矩阵
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine("item " + i + ":");
+            //    for (int j = 0; j < 100; j++)
+            //    {
+            //        Console.Write(SimilarityMatrix[i, j] + "  ");
+            //    }
+            //}
+
+
+
         }
 
         /// <summary>
@@ -123,19 +155,18 @@ namespace RecommendedHelper.ReadDatasetToSQL
         {
             int test_InterestNum = 0;   //测试集上用户兴趣的物品数量
             int totalNum = 0;     //总共命中的物品个数
-            int []count=new int[userTotal];      //cout[i] 为用户的推荐结果和测试集命中物品个数交集
-            double recall = 0.0;    //召回率
-            double precision = 0.0;   //准确率
+            int[] count = new int[userTotal];      //cout[i] 为用户的推荐结果和测试集命中物品个数交集
+          
             for (int i = 0; i < userTotal; i++)
             {
                 for (int j = 0; j < itemTotal; j++)
                 {
-                    if (TestSet[j,i]!=0)  //用户 i 对物品 j 感兴趣
+                    if (TestSet[j, i] != 0)  //用户 i 对物品 j 感兴趣
                     {
                         test_InterestNum++;
                         for (int k = 0; k < recommendNum; k++)
                         {
-                            if (RecommendSet[i,k]==j)    //用户对物品 j 感兴趣，同时也是物品 j 也是推荐物品
+                            if (RecommendSet[i, k] == j)    //用户对物品 j 感兴趣，同时也是物品 j 也是推荐物品
                             {
                                 count[i]++;
                             }
@@ -146,7 +177,7 @@ namespace RecommendedHelper.ReadDatasetToSQL
             }
 
 
-            recall= totalNum / (test_InterestNum * 1.0); // 召回率
+            recall = totalNum / (test_InterestNum * 1.0); // 召回率
             precision = totalNum / (recommendNum * userTotal * 1.0);  //准确率
         }
 
